92a9f3940fde9932a7c3b6f98bdcbf769fa9893d
Tulle12 <72298643+Tulle12@users.noreply.github.com>
2020-10-03 18:02:26 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,30 +1,41 @@
 function USBAusgabe () {
-    let magnstr = 0
-    lightstr = convertToText(input.lightLevel())
-    tempstr = convertToText(input.temperature())
-    loadstr = convertToText(input.soundLevel())
-    str = "" + lightstr + tempstr + magnstr
+    lightstr = nlongstr(6, convertToText(input.lightLevel()))
+    tempstr = nlongstr(6, convertToText(input.temperature()))
+    loadstr = nlongstr(6, convertToText(input.soundLevel()))
+    xstr = nlongstr(6, convertToText(input.acceleration(Dimension.X)))
+    ystr = nlongstr(6, convertToText(input.acceleration(Dimension.Y)))
+    zstr = nlongstr(6, convertToText(input.acceleration(Dimension.Z)))
+    str = "" + lightstr + tempstr + loadstr + xstr + ystr + zstr
     serial.writeLine(str)
 }
 input.onButtonPressed(Button.A, function () {
-    serial.writeLine("Thilo")
-    serial.writeLine("Thilo")
+    serial.writeLine("" + (nlongstr(10, "abc")))
 })
 input.onButtonPressed(Button.B, function () {
-    USBAusgabe()
-    basic.showNumber(input.temperature())
+	
 })
 function nlongstr (num: number, Text: string) {
-    return "" + Text + ";"
+    n = num - Text.length
+    nstr = ""
+    for (let index = 0; index < n; index++) {
+        nstr = "" + nstr + " "
+    }
+    return "" + nstr + Text + ";"
 }
+let nstr = ""
+let n = 0
 let str = ""
+let zstr = ""
+let ystr = ""
+let xstr = ""
 let loadstr = ""
 let tempstr = ""
 let lightstr = ""
-basic.setLedColor(0xff0000)
+basic.setLedColor(0x999999)
 serial.setBaudRate(BaudRate.BaudRate115200)
 serial.redirectToUSB()
 basic.clearScreen()
 basic.forever(function () {
-	
+    USBAusgabe()
+    basic.pause(100)
 })
